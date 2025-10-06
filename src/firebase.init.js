694c6adc112341d1ab04668260c43bfbc3a4b85b
jerinjamandev <jerinjaman.dev@gmail.com>
2025-10-06 22:34:40 +0600

    add

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0TWzX1grgMYCpB6U92Y4AXWuQ1---D8A",
+  authDomain: "resturent-2bbd9.firebaseapp.com",
+  projectId: "resturent-2bbd9",
+  storageBucket: "resturent-2bbd9.firebasestorage.app",
+  messagingSenderId: "920328450899",
+  appId: "1:920328450899:web:e4450b2e8ed85be3e867a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
